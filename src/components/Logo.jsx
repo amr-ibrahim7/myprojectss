963eda67bccfc54d76dcc0d6ef9b218de43cdf7b
amr-ibrahim7 <amrnew100@gmail.com>
2024-12-03 01:17:11 +0200

    add api3

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,9 +1,31 @@
+// import React from "react";
+// import { Link } from "react-router-dom";
+
+// const Logo = () => {
+//   return (
+//     <Link rel="preload" to="/" as="image" aria-label="Home">
+//       <img
+//         src="/image/LogoMe1.webp"
+//         // srcSet="/image/LogoMe11-medium.webp 320w, /image/LogoMe11-large.webp 640w"
+//         // sizes="(max-width: 768px) 320px, (min-width: 769px) 640px"
+//         width={160}
+//         height={55}
+//         alt="Company logo"
+//         loading="eager" // تحميل فوري
+//         fetchpriority="high" // أولوية تحميل عالية
+//       />
+//     </Link>
+//   );
+// };
+
+// export default React.memo(Logo);
+
 import React from "react";
 import { Link } from "react-router-dom";
 
 const Logo = () => {
   return (
-    <Link rel="preload" to="/" as="image" aria-label="Home">
+    <Link to="/" aria-label="Home">
       <img
         src="/image/LogoMe1.webp"
         // srcSet="/image/LogoMe11-medium.webp 320w, /image/LogoMe11-large.webp 640w"
@@ -11,8 +33,7 @@ const Logo = () => {
         width={160}
         height={55}
         alt="Company logo"
-        loading="eager" // تحميل فوري
-        fetchpriority="high" // أولوية تحميل عالية
+        className="mr-[-1rem] md:mr-0"
       />
     </Link>
   );
