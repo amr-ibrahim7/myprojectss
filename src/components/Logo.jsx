4c11415a200fa4a36a0402429572bd16324dbc2f
amr-ibrahim7 <amrnew100@gmail.com>
2024-12-03 02:06:51 +0200

    edit images

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -28,8 +28,8 @@ const Logo = () => {
     <Link to="/" aria-label="Home">
       <img
         src="/image/LogoMe1.webp"
-        // srcSet="/image/LogoMe11-medium.webp 320w, /image/LogoMe11-large.webp 640w"
-        // sizes="(max-width: 768px) 320px, (min-width: 769px) 640px"
+        srcSet="/image/LogoMe11-medium.webp 320w, /image/LogoMe11-large.webp 640w"
+        sizes="(max-width: 768px) 320px, (min-width: 769px) 640px"
         width={160}
         height={55}
         alt="Company logo"
